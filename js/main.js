ca89afa79e27005aa3bdd80abe347d968fd9718e
Juan Aramburo <2020030187@upsin.edu.mx>
2024-10-18 22:36:40 -0700

    Diseño de la cotizacion

--- js/main.js
+++ js/main.js
@@ -92,8 +92,8 @@ const btnLimpiar3 = document.getElementById("btnLimpiar3");
 const labelError3 = document.getElementById("error3");
 
 function calcular2(){
-    const opcion2 = document.getElementById("autoOpcion").value;
-    let val = document.getElementById("idValorAuto").value;
+    const opcion2 = document.getElementById("cmbplanes").value;
+    let val = document.getElementById("txtValor").value;
     labelError3.textContent="";
     
     let opc2 = parseFloat(opcion2);
@@ -106,7 +106,7 @@ function calcular2(){
     }
     else{
         let enganche = val * 0.3;
-        document.getElementById("idEnganche").value = enganche;
+        document.getElementById("txtEnganche").value = enganche;
         let totalen = val - enganche;
         switch(opc2){
             case 1: totalfin = (val * 0.125) + totalen; break;
@@ -115,7 +115,7 @@ function calcular2(){
             case 4: totalfin = (val * 0.26) + totalen;break;
             case 5: totalfin = (val * 0.45) + totalen;break;
         }
-        document.getElementById("idFinanciar").value = totalfin;
+        document.getElementById("txtFinanciar").value = totalfin;
         switch(opc2){
             case 1: pagomensual = totalfin / 12;break;
             case 2: pagomensual = totalfin / 18;break;
@@ -123,15 +123,15 @@ function calcular2(){
             case 4: pagomensual = totalfin / 36;break;
             case 5: pagomensual = totalfin / 48;break;
         }
-        document.getElementById("idMensual").value = pagomensual.toFixed(2);
+        document.getElementById("txtPago").value = pagomensual.toFixed(2);
     }
 };
 
 function limpiar3(){
-    document.getElementById("idValorAuto").value = "";
-    document.getElementById("idEnganche").value = "";
-    document.getElementById("idFinanciar").value = "";
-    document.getElementById("idMensual").value = "";
+    document.getElementById("txtValor").value = "";
+    document.getElementById("txtEnganche").value = "";
+    document.getElementById("txtFinanciar").value = "";
+    document.getElementById("txtPago").value = "";
     labelError.textContent="";
 }
 
