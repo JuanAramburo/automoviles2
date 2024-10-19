0728825ae71711007c7d47455ac1a19f6cb56e0d
Juan Aramburo <2020030187@upsin.edu.mx>
2024-10-18 22:28:31 -0700

    Generar una nueva tarjeta

--- js/main.js
+++ js/main.js
@@ -85,3 +85,56 @@ btnLimpiar2.addEventListener('click', function limpiar2(){
     document.getElementById("idResultado").value = "";
     labelError2.textContent="";
 });
+
+//Cotizacion
+const btnCalcular3 = document.getElementById("btnCalcular3");
+const btnLimpiar3 = document.getElementById("btnLimpiar3");
+const labelError3 = document.getElementById("error3");
+
+function calcular2(){
+    const opcion2 = document.getElementById("autoOpcion").value;
+    let val = document.getElementById("idValorAuto").value;
+    labelError3.textContent="";
+    
+    let opc2 = parseFloat(opcion2);
+    let totalfin = 0;
+    let pagomensual = 0;
+
+    //validar
+    if(!val){
+        labelError3.textContent="Inserta el valor del automovil";
+    }
+    else{
+        let enganche = val * 0.3;
+        document.getElementById("idEnganche").value = enganche;
+        let totalen = val - enganche;
+        switch(opc2){
+            case 1: totalfin = (val * 0.125) + totalen; break;
+            case 2: totalfin = (val * 0.172) + totalen;break;
+            case 3: totalfin = (val * 0.21) + totalen;break;
+            case 4: totalfin = (val * 0.26) + totalen;break;
+            case 5: totalfin = (val * 0.45) + totalen;break;
+        }
+        document.getElementById("idFinanciar").value = totalfin;
+        switch(opc2){
+            case 1: pagomensual = totalfin / 12;break;
+            case 2: pagomensual = totalfin / 18;break;
+            case 3: pagomensual = totalfin / 24;break;
+            case 4: pagomensual = totalfin / 36;break;
+            case 5: pagomensual = totalfin / 48;break;
+        }
+        document.getElementById("idMensual").value = pagomensual.toFixed(2);
+    }
+};
+
+function limpiar3(){
+    document.getElementById("idValorAuto").value = "";
+    document.getElementById("idEnganche").value = "";
+    document.getElementById("idFinanciar").value = "";
+    document.getElementById("idMensual").value = "";
+    labelError.textContent="";
+}
+
+btnCalcular3.addEventListener('click', calcular2);
+btnLimpiar3.addEventListener('click', limpiar3);
+
