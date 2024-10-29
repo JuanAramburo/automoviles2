3bfe1d04e30958cd2a6998fac852e6fe322a659d
Juan Aramburo <2020030187@upsin.edu.mx>
2024-10-29 16:49:49 -0700

    Actualizacion de pagina Automoviles

--- js/automoviles.js
+++ js/automoviles.js
@@ -1,190 +1,71 @@
-const data = [
-    {
-        "marca":"TOYOTA",
-        "modelo":"Hilux 2024",
-        "imagen":"/img/toyota1.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"TOYOTA",
-        "modelo":"SW Diamond 2024",
-        "imagen":"/img/toyota2.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Credito 20% Pago Inicial Hasta 60 Meses",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"TOYOTA",
-        "modelo":"Hilux GR Sport 2024",
-        "imagen":"/img/toyota3.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo, Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"TOYOTA",
-        "modelo":"Camry 2024",
-        "imagen":"/img/toyota4.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"NISSAN",
-        "modelo":"GT-R Nismo 2024",
-        "imagen":"/img/nissan1.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"NISSAN",
-        "modelo":"Safari 2024",
-        "imagen":"/img/nissan2.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"NISSAN",
-        "modelo":"Micra 2024",
-        "imagen":"/img/nissan3.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Estandar, Basico para empezar. Credito 20% Pago Inicial Hasta 60 Meses",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"NISSAN",
-        "modelo":"Safari 2024",
-        "imagen":"/img/nissan4.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, rines de lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"FORD",
-        "modelo":"Fiesta 2024",
-        "imagen":"/img/ford1.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Credito 20% Pago Inicial Hasta 60 Meses de Plazo sin intereses",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"FORD",
-        "modelo":"Focus 2024",
-        "imagen":"/img/ford2.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"FORD",
-        "modelo":"Explorer 2024",
-        "imagen":"/img/ford3.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo ",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"FORD",
-        "modelo":"Mustang 2024",
-        "imagen":"/img/ford4.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"HONDA",
-        "modelo":"Civic TyperR 2024",
-        "imagen":"/img/honda1.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles" 
-    },{
-        "marca":"HONDA",
-        "modelo":"Vezel 2024",
-        "imagen":"/img/honda2.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"HONDA",
-        "modelo":"e 2024",
-        "imagen":"/img/honda3.jpg",
-        "descripcion":"Carro Electrico, Energetico, Silencioso, Rines de Lujo y rapido a Credito 20% Pago Incial Hasta 60 Meses de plazo sin intereses",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"HONDA",
-        "modelo":"S2000",
-        "imagen":"/img/honda4.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo ",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"CHEVROLET",
-        "modelo":"Aveo 2024",
-        "imagen":"/img/chevrolet1.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo sin intereses",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"CHEVROLET",
-        "modelo":"D-Max Hi-Ride 2024",
-        "imagen":"/img/chevrolet2.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo ",
-        "mas":"Mas Detalles" 
-    },{
-        "marca":"CHEVROLET",
-        "modelo":"Silverado 2024",
-        "imagen":"/img/chevrolet3.jpg",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    },{
-        "marca":"CHEVROLET",
-        "modelo":"Joy 2024",
-        "imagen":"/img/chevrolet4.png",
-        "descripcion":"Aire Acondicionado, Transmision Automatica, Rines de Lujo. Credito 20% Pago Inicial Hasta 60 Meses de Plazo",
-        "mas":"Mas Detalles"
-    }
-];
-
-window.addEventListener("DOMContentLoaded", () => {
-    crearCards();
+window.addEventListener("DOMContentLoaded", async (event) => {
+    await crearCards();
     crearFooter();
 });
 
-function crearCards() {
-    const section = document.createElement("section");
-    section.className = "contenedorflexAIC";
+async function crearCards() {
+    try{
+        const response = await fetch("/js/automoviles.json");
+        const data = await response.json();
+        const section = document.createElement("section");
+        section.className = "contenedorflexAIC";
 
-    const marcas = {};
-    data.forEach(item => {
-        if (!marcas[item.marca]) {
-            marcas[item.marca] = [];
-        }
-        marcas[item.marca].push(item);
-    });
-
-    for (const marca in marcas) {
-        const marcaContainer = document.createElement("div");
-        marcaContainer.className = "marca-container";
-        marcaContainer.id = marca.toLowerCase();
-
-        const tituloMarca = document.createElement("h2");
-        tituloMarca.className = `titulo-${marca.toLowerCase()}`;
-        tituloMarca.textContent = marca;
-        marcaContainer.appendChild(tituloMarca);
-
-        const cardsContainer = document.createElement("div");
-        cardsContainer.className = "cards-container";
-
-        marcas[marca].forEach(car => {
-            const item = document.createElement("div");
-            item.className = "card";
-
-            const cardImg = document.createElement("img");
-            cardImg.src = car.imagen;
-            cardImg.alt = car.modelo;
-            cardImg.width = "100%";
-            item.appendChild(cardImg);
-
-            const cardH1 = document.createElement("h1");
-            cardH1.textContent = car.modelo;
-            item.appendChild(cardH1);
-
-            const cardP = document.createElement("p");
-            cardP.className = "texto";
-            cardP.textContent = car.descripcion;
-            item.appendChild(cardP);
-
-            const cardBtn = document.createElement("button");
-            cardBtn.textContent = car.mas;
-            item.appendChild(cardBtn);
-
-            cardsContainer.appendChild(item);
+        const marcas = {};
+        data.forEach(item => {
+            if (!marcas[item.marca]) {
+                marcas[item.marca] = [];
+            }
+            marcas[item.marca].push(item);
         });
 
-        marcaContainer.appendChild(cardsContainer);
-        section.appendChild(marcaContainer);
+        for (const marca in marcas) {
+            const marcaContainer = document.createElement("div");
+            marcaContainer.className = "marca-container";
+            marcaContainer.id = marca.toLowerCase();
+
+            const tituloMarca = document.createElement("h2");
+            tituloMarca.className = `titulo-${marca.toLowerCase()}`;
+            tituloMarca.textContent = marca;
+            marcaContainer.appendChild(tituloMarca);
+
+            const cardsContainer = document.createElement("div");
+            cardsContainer.className = "cards-container";
+
+            marcas[marca].forEach(car => {
+                const item = document.createElement("div");
+                item.className = "card";
+
+                const cardImg = document.createElement("img");
+                cardImg.src = car.imagen;
+                cardImg.alt = car.modelo;
+                cardImg.width = "100%";
+                item.appendChild(cardImg);
+
+                const cardH1 = document.createElement("h1");
+                cardH1.textContent = car.modelo;
+                item.appendChild(cardH1);
+
+                const cardP = document.createElement("p");
+                cardP.className = "texto";
+                cardP.textContent = car.descripcion;
+                item.appendChild(cardP);
+
+                const cardBtn = document.createElement("button");
+                cardBtn.textContent = car.mas;
+                item.appendChild(cardBtn);
+
+                cardsContainer.appendChild(item);
+            });
+
+            marcaContainer.appendChild(cardsContainer);
+            section.appendChild(marcaContainer);
+        }
+
+        document.body.appendChild(section);
+    }catch(error){
+        console.error("Error al cargar datos", error);
     }
-
-    document.body.appendChild(section);
 }
-
 function crearFooter() {
     const footer = document.createElement("footer");
     footer.id = "pie";
